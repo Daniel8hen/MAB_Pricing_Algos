730a14255b3f07a8a0907592fee9e12b95e5d09a
daniel8hen <daniel8hen@gmail.com>
2021-05-18 14:30:53 +0300

    Works on synthetic data

--- src/real_data_desired_win_rate_reward.py
+++ src/real_data_desired_win_rate_reward.py
@@ -39,6 +39,7 @@ class BetaBernoulli(ProbabilityModel):  ### This class inheretes from Probabilit
         y = np.array(y)
         self.T += (y == 1).sum()
         self.F += (y == 0).sum()
+
         return self
 
     def discount(self, r):
@@ -76,9 +77,13 @@ class BiddingStrategy():
             model.discount(r)
         return self
 
-    def parameters(self):
+    def parameters(self, return_type=None):
         """BetaBernoulli based - parameters of the model: alpha + beta"""
         """TODO: Generalize for general models"""
+        if return_type=="T":
+            return {price: model.T for price, model in self.bid_model.items()}
+        elif return_type=="F":
+            return {price: model.F for price, model in self.bid_model.items()}
         return {price: model.T / (model.T + model.F) for price, model in self.bid_model.items()}
 
     def bid(self, context):
@@ -105,9 +110,10 @@ class BiddingStrategy():
         reward_arr = []
         for price in self.bid_model.keys():
             r = self.specific_reward(price, bid_price, won)
-            if r != 0:
-                for _ in range(abs(r)):
-                    self.bid_model[price].partial_fit([context], [r > 0])
+            if r > 0:
+                self.bid_model[price].partial_fit([context], [True])
+            if r < 0:
+                self.bid_model[price].partial_fit([context], [False])
             reward_arr.append(r)
         return reward_arr
 
@@ -124,12 +130,17 @@ class BiddingStrategy():
         On top of that, exponent and that is the returned value
         The goal is to have the highest value a very high value, and the lowest - 1"""
         arr = np.array(arr)
-        arr = np.abs(np.log(arr) - np.log(self.desired_win_rate)) # 1L1D
+        # print("arr is:", arr)
+        # arr = np.abs(np.log(arr) - np.log(self.desired_win_rate)) # 1L1D
+        # overwrite for test
+        # arr = 1/(np.round(np.abs(arr - self.desired_win_rate),1) + np.float(0.01)) # numerical stability
+        arr = 1 / (np.abs(arr - self.desired_win_rate) + np.float(0.00001))  # numerical stability
+        # print("arr after manipulation and distance:", arr)
+        # print("arr for sum is:", arr/arr.sum())
+        return arr/arr.sum()
+
         arr = arr - np.min(arr)
         # The goal: The closer you are as a bandit to the win rate, meaning this bandit is more likely to be chosen
-
-        # log_arr = np.log(arr)
-        # normalized_log_arr = 2 * log_arr - np.min(2 * log_arr)
         exp_norm_arr = np.exp(arr)
         exp_norm_arr /= exp_norm_arr.sum()
 
@@ -145,14 +156,27 @@ class BiddingStrategy():
             win = b > auction_bid
 
             regret_arr.append(np.abs(b - auction_bid)) # log regret: ABS(bid - constant, which is the "win price by default")
+
             self.discount(discount_perc) # discount
             r = self.reward(bid_price=b, won=win, context=[1])  # Generate reward per auction fin.
 
-
         return np.mean(np.array(regret_arr))
 
-    # TODO: We want a negative reward
 
+    def simulate_by_real_dataset(self, df, discount_perc):
+        """Going to simulate by real df"""
+        regret_arr = []
+
+        for i, row in df.iterrows():
+            b = self.bid([i])  # provide a bid
+            # b = random.random()
+            auction_bid = row['bidPrice']
+            win = b > auction_bid
+
+            regret_arr.append(np.abs(b - auction_bid))  # log regret: ABS(bid - constant, which is the "win price by default")
+            self.discount(discount_perc)  # discount
+            r = self.reward(bid_price=b, won=win, context=[1])  # Generate reward per auction fin.
+        return regret_arr, np.mean(np.array(regret_arr))
 
 #       def specific_reward_function(self, bandit_price, bid_price, won):
 #         """This method will update per each bandit its reward based on similiarity logic (how close were they to win / lose)"""
@@ -180,168 +204,30 @@ class BiddingStrategy():
 
 # COMMAND ----------
 
-cls = BetaBernoulli(1, 1000)  # Defining an instance of this BetaBernoulli with 1 win, 1000 loses as our prior
 
-# COMMAND ----------
-
-# COMMAND ----------
-
-reward_dict = {}
-max_data_point = 10  # max number of bins
-best_regret = 100
-best_params = {}
-penalties = [-1000, -1, 0, 10, 1000]  # TODO: try to think on functions / many distinct values -
-
-# TODO:
-# (1) joblib - multiprocessing https://joblib.readthedocs.io/en/latest/parallel.html for the below
-# (2):
+# Main
 from joblib import delayed, Parallel
 
+if __name__ == '__main__':
+    import pandas as pd
+    from datetime import datetime
 
-@delayed
-def run_iteration():
-    for comb in itertools.product(range(1, max_data_point + 1), [-2, -1, 1, 2], [True, False]):
-        reward_dict[comb] = random.choice(penalties)
-    biddingStrategy = BiddingStrategy(n_bins=max_data_point, max_bid=max_data_point, priors=range(max_data_point),
+    cls = BetaBernoulli(1, 1)  # Defining an instance of this BetaBernoulli with 1 win, 1000 loses as our prior
+
+    df = pd.read_csv("../data/data.csv")
+    df = df.query("placementType == 'banner'")
+    max_data_point = 1  # max number of bins
+    n_bins=10
+
+    format = "%m/%d/%Y, %H:%M:%S"
+    print("Start:", datetime.now().strftime(format))
+    biddingStrategy = BiddingStrategy(n_bins=n_bins, max_bid=max_data_point, priors=[1]*n_bins,
                                       classifier=cls, desired_win_rate=0.6)
-    regret = biddingStrategy.simulate_by_constant(4, 0.99)
-    return regret, reward_dict
+    # regret = biddingStrategy.simulate_by_constant(4, 0.99)
+    regret = biddingStrategy.simulate_by_real_dataset(df.head(2000), 0.99)
+    print("Regret:", regret)
 
-
-from datetime import datetime
-
-format = "%m/%d/%Y, %H:%M:%S"
-print("Start:", datetime.now().strftime(format))
-lst = Parallel(n_jobs=8)(run_iteration() for i in range(1000))
-best_regret, best_params = min(lst)
-print("Best Regret:", best_regret)
-# print("best params:", best_params)
-reward_arr = []
-for k, v in best_params.items():
-    reward_arr.append((k[0], k[1], k[2], v))
-pandas_best_params = pd.DataFrame(reward_arr, columns=["bandit_price", "diff", "won", "reward"]).astype(int)
-pandas_best_params.to_csv("output.csv", index=None)
-print("End:", datetime.now().strftime(format))
-
-# COMMAND ----------
-
-# best_regret = 1.4006734006734005 for diff binary
-# best_regret = 1.1 with 4 directions diff (-2, -1, 1, 2)
-# best_regret = 0.919 with diff (4 directions) / customized reward [-10,-5,-2,-1,0,1,2,5,10]
-# best_regret = 0.39 with diff (4 directions) / customized super reward penalties = [-1000,-100,-50,-20,-1,0,1,20,50,100,1000] - didn't manage to reproduce that specific result, mostly due to randomness
-best_regret
-
-# COMMAND ----------
-
-best_params
-# (bid_price, diff, won)
-# bid price was 1, bid_price > bandit_price, lost => up
-
-# diff
-# if 0 < bid_price - bandit_price <= 2:
-#         diff = 1
-#       elif -2 < bid_price - bandit_price <= 0:
-#         diff = -1
-#       elif bid_price - bandit_price > 2:
-#         diff = 2
-#       else:
-#         diff = -2
-
-# COMMAND ----------
-
-# def specific_reward_by_dict(self, bid_price, bandit_price, won):
-#       """This method generate a reward based on bid price, bandit price, won events, stored in a reward_dictionary outside"""
-#       diff = (bid_price - bandit_price) > 0 # Boolean flag
-#       return int(self.reward_dict.get(bid_price, diff, won), 0)
-
-
-# COMMAND ----------
-
-
-# dummy_X = np.random.randint(low=1, high=max_data_point, size=(10,1)) # Dummy data - represents the bids that were in many auctions
-# print(dummy_X) # e.g. 3 = banner, dntTrue, iOS
-
-# dummy_bids = np.random.randint(low=1, high=max_data_point, size=(10,)) #
-# print(dummy_bids)
-
-# dummy_bool_auction_results = np.random.rand((10)) > 0.5
-# print(dummy_bool_auction_results)
-
-
-# cls = SGDClassifier(loss='log')
-# cls.partial_fit(dummy_X, )
-# cls.fit(dummy_X, np.random.rand(dummy_X.shape[0]) > 0.5)
-# cls.partial_fit()
-
-
-# reward_arr = []
-# bids_arr = []
-# win_lose_arr = []
-# probs_arr = []
-# params_arr = []
-# for i in range(1, 1000):
-#   b = biddingStrategy.bid([i])
-#   params_arr.append(list(biddingStrategy.parameters().values()))
-#   print(f"My bid is: {b:0.3f}")
-#
-#   bids_arr.append(b)
-#   win = b > 7
-#   win_lose_arr.append(win)
-#
-#   biddingStrategy.discount(0.9)
-#
-#   r = biddingStrategy.reward(bid_price=b, won=win, context=[1])
-#   probs_arr.append(biddingStrategy.probabilities)
-#   reward_arr.append(r)
-#
-# # COMMAND ----------
-#
-# for i in range(0, 1000, 10):
-#   plt.bar(biddingStrategy.bid_model.keys(),params_arr[i])
-#   plt.show()
-#
-# # COMMAND ----------
-#
-# pd.DataFrame(bids_arr).rolling(10).mean().plot()
-# # pd.DataFrame(reward_arr).astype(int).plot()
-# pd.DataFrame(probs_arr).plot()
-# plt.ylim(-1e-3, 1e-3)
-
-# COMMAND ----------
-
-# print(np.array(probs_arr).max())
-# print(np.array(probs_arr).min())
-
-# COMMAND ----------
-
-# probs_arr
-
-# COMMAND ----------
-
-# probabilities = np.array([0.000456890760588994,
-#   0.006465069776677389,
-#   0.0011343878313113415,
-#   0.0055366061701542955,
-#   0.003659762936292841,
-#   0.0025525817626757936,
-#   0.003517560887148804,
-#   0.0004356916625517895,
-#   0.0009289473262418103,
-#   0.0005443335700367749,
-#   0.0011412011710087097,
-#   0.0011297464941493651])
-
-# COMMAND ----------
-
-# prob_hund = np.array(probabilities)*100
-
-# COMMAND ----------
-
-# probabilities = customized_softmax(np.array(prob_hund))
-# print(probabilities)
-
-# COMMAND ----------
-
-# n_samples = 1000
-# print(list(zip(biddingStrategy.bid_model.keys(), probabilities/np.sum(probabilities))))
-# print(np.random.choice(a=list(biddingStrategy.bid_model.keys()), p=probabilities/np.sum(probabilities), size=n_samples))
+    ### General TODOs:
+    # 1. Plotting each aspect of pricing here so we can see how the model is learning
+    # 2. No context - making it work by heavily debugging
+    # 3. With context? Adding it may help
