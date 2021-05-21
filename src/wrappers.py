c1b483148082a955735548a62a89172b5f2445ba
Uri Goren <uri@goren.ml>
2021-05-22 02:34:03 +0300

    playing with contextual bandits

--- src/wrappers.py
+++ src/wrappers.py
@@ -1,5 +1,7 @@
 import numpy as np
 from types import MethodType
+from sklearn.ensemble import BaggingClassifier
+from copy import deepcopy as clone
 
 class decision_function_from_proba:
     def __init__(self, model):
@@ -10,3 +12,11 @@ class decision_function_from_proba:
             return -np.log(proba)
         self.model.decision_function=MethodType(new_f, self.model)
         return self.model
+
+class TemporalBagging(BaggingClassifier):
+    def __init__(self, base_classifier, n_estimators, **kwargs)
+        super().__init__(base_classifier, n_estimators, **kwargs)
+        self.current_index = 0
+    def partial_fit(self, X, y):
+        self.estimators_[self.current_index].fit(X,y)
+        self.current_index = (self.current_index + 1) % len(self.estimators_)
