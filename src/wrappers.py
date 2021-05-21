cbdb6565e17a7322d1f109bb92642d34196150c4
Uri Goren <uri@goren.ml>
2021-05-21 21:39:31 +0300

    playing with contextual bandits

---
+++ src/wrappers.py
@@ -0,0 +1,12 @@
+import numpy as np
+from types import MethodType
+
+class decision_function_from_proba:
+    def __init__(self, model):
+        self.model = model
+    def __call__(self, f):
+        def new_f(this, X):
+            proba=f(this.predict_proba(X))
+            return -np.log(proba)
+        self.model.decision_function=MethodType(self.model,new_f)
+        return self.model
